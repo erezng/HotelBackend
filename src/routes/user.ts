d17773e29707fa80891a3b50b09761d6ba7f8ebf
erez gabay <erez2g@gmail.com>
2023-02-08 21:08:38 +0200

    userrouter

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,7 +3,8 @@ import _ from "underscore";
 import bcrypt from "bcryptjs";
 import { User } from "../db/models/user.js";
 import { Role } from "../db/models/role.js";
-
+import Jwt from "jsonwebtoken";
+import authConfig from "../db/config/auth.config.js";
 const router = Router();
 router.post("/signup", async (req, res) => {
   const body = _.pick(req.body, "username", "email", "password");
@@ -18,8 +19,34 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-// router.post("/sigup",async(req,res)=>{
-//     try{
-//         const user=await User.findOne({})
-//     }
-// })
+router.post("/sigin", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email }).populate<{
+      roles: Array<typeof Role>;
+    }>("roles");
+    if (!user) return res.status(401).json({ message: "User not found!" });
+    const isPasswordVaild = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordVaild)
+      return res.status(401).json({ message: "Invaild Credentials!" });
+    const token = Jwt.sign({ id: user.id }, authConfig.secret, {
+      expiresIn: "30d",
+    });
+    const authorities = [];
+    for (let i = 0; i < user.roles.length; i++) {
+      authorities.push(`ROLE_` + user.roles[i].name.toUpperCase());
+    }
+    return res.status(200).json({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      roles: authorities,
+      accessToken: token,
+    });
+  } catch (e) {
+    return res.status(500).json({ message: "server error", error: e });
+  }
+});
+export { router as authRouter };
