78b2d1352ab30ea21004a2f6bd9aba009a09ac6c
erez gabay <erez2g@gmail.com>
2023-02-08 22:13:09 +0200

    connectDB

--- src/db/config/db.config.ts
+++ src/db/config/db.config.ts
@@ -2,4 +2,5 @@ export default {
   HOST: "127.0.0.1",
   PORT: 27017,
   DB: "Hotels",
+  ROLES: ["admin", "moderator", "user"],
 };
--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,11 +1,25 @@
 import mongoose from "mongoose";
 import dbConfig from "./config/db.config.js";
+import { Role } from "./models/role.js";
 
-const { HOST, DB, PORT } = dbConfig;
+const { HOST, DB, PORT, ROLES } = dbConfig;
 const connect = async () => {
   //mongoose 7 update:
   mongoose.set("strictQuery", false);
   await mongoose.connect(`mongodb://${HOST}:${PORT}/${DB}`);
   console.log(`Succesfully connected to the database ${DB}`);
+  initDB();
+};
+const initDB = () => {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      ROLES.map((s) => new Role({ name: s })).forEach((role) => {
+        role.save((err) => {
+          if (err) console.log(err);
+          else console.log("added", role.name, "to roles colection");
+        });
+      });
+    }
+  });
 };
 export { connect };
