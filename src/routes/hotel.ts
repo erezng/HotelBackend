e03af8002be389966e8d4bfd87f6c8bcb45fefe9
erez gabay <erez2g@gmail.com>
2023-02-08 20:04:19 +0200

    routerchange

--- src/routes/hotel.ts
+++ src/routes/hotel.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { Hotel } from "../models/hotel.js";
-import _ from "underscore"
+import _ from "underscore";
 const router = Router();
 router.post("/", (req, res) => {
   const body = _.pick(
@@ -14,15 +14,15 @@ router.post("/", (req, res) => {
   );
   const hotel = new Hotel(body);
   hotel
-  .save()
-  .then((saved) => {
-    res.json({ message: "Saved successfully"}),
-    // id: saved._id,
-    // hotel:saved
-  })
-  .catch((e)=>{
-    res.status(500).json({message:`Error:${e}`})
-  });
+    .save()
+    .then(
+      (saved) => res.json({ message: "Saved successfully" })
+      // id: saved._id,
+      // hotel:saved
+    )
+    .catch((e) => {
+      res.status(500).json({ message: `Error:${e}` });
+    });
 });
 
-export {router as hotelRouter}
+export { router as hotelRouter };
